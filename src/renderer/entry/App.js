0912b0806db21a23db3b052b0b2e90c256341e0f
lmn_741 <liu435839449>
2024-04-16 21:31:27 +0800

    init pro

--- src/renderer/entry/App.js
+++ src/renderer/entry/App.js
@@ -185,10 +185,6 @@ function App() {
 		    }}
 		    footerRender={() => (
 			    <DefaultFooter
-				    links={[
-					    { key: 'test', title: 'layout', href: 'www.alipay.com' },
-					    { key: 'test2', title: 'layout2', href: 'www.alipay.com' },
-				    ]}
 				    copyright="这是一条测试文案"
 			    />
 		    )}
