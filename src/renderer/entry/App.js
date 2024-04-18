362022e70295717ee50701b914f6ef7bd4de647c
lmn_741 <liu435839449>
2024-04-18 20:10:31 +0800

    init pro

--- src/renderer/entry/App.js
+++ src/renderer/entry/App.js
@@ -52,13 +52,41 @@ function App() {
 			webIns.openDevTools();
 			// webIns.executeJavaScript(webviewScripts.getScript(_.toNumber(data?.systemId), data?.account, data?.password));\
 			webIns.executeJavaScript(`
-			
+				setTimeout(()=>{
+					var doc = document;
+					var exploreFeeds = doc.getElementById("exploreFeeds")
+					var sections = doc.getElementsByClassName("note-item");
+					console.log(sections, "sections=========")
+					for (var i=0;i<sections.length;i++){
+						const section = sections[i];
+						var footer = section.querySelector(".footer");
+						var title = footer.querySelector(".title");
+						var titleSpan = footer.querySelector("span");
+						// 1、获取小红书标题
+						const textContent = titleSpan?.textContent;
+						// 2、获取每个帖子图片、帖子地址、帖子详情
+						var aElement = section.querySelector("a.mask");
+						var aImg = aElement.querySelector("img");
+						var imgSrc = aImg.src;      //帖子图片
+						var aHref = aElement.href;  //帖子详情
+						var styleObj = aElement.style;
+						console.log(aElement, '9999', aHref, '555', styleObj, '000', imgSrc)
+					}
+				}, 10000)
 			`)
 		});
 	}, [])
 	
 	return (
-		<webview id="webview" src={"https://www.zhihu.com/"}/>
+		<webview
+			id="webview"
+			nodeintegration="yes"
+			allowpopups="yes"
+			nodeintegrationinsubframes="yes"
+			allowRunningInsecureContent="yes"
+			disablewebsecurity="yes"
+			webpreferences="contextIsolation=no"
+			src={"https://www.xiaohongshu.com/explore"}/>
 	)
 }
 
