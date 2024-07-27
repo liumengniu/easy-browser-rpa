bf7fefc3730c6bbd6575b8c3c5a938a1f2c253e4
lmn_741 <liu435839449>
2024-07-27 22:15:52 +0800

    修改脚本

--- src/renderer/pages/BossZhiPin/index.js
+++ src/renderer/pages/BossZhiPin/index.js
@@ -13,20 +13,23 @@
 import {memo, useEffect, useMemo, useState} from "react";
 import webviewScripts from "@/scripts";
 import {useLocation} from "react-router-dom";
+import "./index.less"
+import {Button, Form, Input, Select} from "antd";
 
 function BossZhiPin() {
 	const location = useLocation();
 	const [path, setPath] = useState(null);
+	const [form] = Form.useForm()
 	
 	
 	useEffect(() => {
-		console.log("执行次数")
-		if(!path) return
-		let webIns = document.getElementById('webview2');
-		webIns.addEventListener('did-finish-load', () => {
-			webIns.openDevTools();
-			webIns.executeJavaScript(webviewScripts?.bossScript);
-		});
+		// console.log("执行次数")
+		// if(!path) return
+		// let webIns = document.getElementById('webview2');
+		// webIns.addEventListener('did-finish-load', () => {
+		// 	webIns.openDevTools();
+		// 	webIns.executeJavaScript(webviewScripts?.bossScript);
+		// });
 	}, [path])
 	
 	useEffect(() => {
@@ -43,23 +46,61 @@ function BossZhiPin() {
 		setPath(fileAbPath)
 	}
 	
+	const options = [
+		{label: '本地磁盘', value: '本地磁盘'},
+		{label: '本地数据库', value: '本地数据库'}
+	]
+	
+	/**
+	 * 数据采集
+	 */
+	const handleCollection = async () =>{
+		try {
+			const values = await form.validateFields();
+			console.log(values, '======values===========')
+			if (!_path) return;
+			let webIns = document.getElementById('webview');
+			webIns.openDevTools();
+			webIns.executeJavaScript(webviewScripts?.xiaohongshuScript(values?.type), true);
+		}catch (e) {
+			console.log(e, '======eeeeeeeeeeeeeeeeeeeeeeee===========')
+		}
+	}
+	
 	return (
-		<>
-			{
-				path && <webview
-					id="webview2"
-					className="webview"
-					nodeintegration="yes"
-					allowpopups="yes"
-					nodeintegrationinsubframes="yes"
-					allowRunningInsecureContent="yes"
-					disablewebsecurity="yes"
-					webpreferences="contextIsolation=no"
-					src={location?.state?.src}
-					preload={path}
-				/>
-			}
-		</>
+		<div className="xiaohongshu">
+			<div  className="xiaohongshu-options">
+				<Form form={form}>
+					<Form.Item label="选择流程(开发中)" name="process">
+						<Input />
+					</Form.Item>
+					<Form.Item label="存储形式" name="type" rules={[{required: true, message: '请选择存储形式!'}]}>
+						<Select options={options}/>
+					</Form.Item>
+					<Form.Item wrapperCol={{offset: 8, span: 16,}}>
+						<Button type="primary" onClick={handleCollection}>
+							开始采集
+						</Button>
+					</Form.Item>
+				</Form>
+			</div>
+			<div className="xiaohongshu-webview">
+				{
+					path && <webview
+						id="webview2"
+						className="webview"
+						nodeintegration="yes"
+						allowpopups="yes"
+						nodeintegrationinsubframes="yes"
+						allowRunningInsecureContent="yes"
+						disablewebsecurity="yes"
+						webpreferences="contextIsolation=no"
+						src={location?.state?.src || "https://www.zhipin.com/"}
+						preload={path}
+					/>
+				}
+			</div>
+		</div>
 	)
 }
 
