65dcc1b4aa3b7412bbb515109516a60d8669b74d
lmn_741 <liu435839449>
2024-07-29 22:06:06 +0800

    修改编译配置

--- src/renderer/layout/navbar/SideBar.js
+++ src/renderer/layout/navbar/SideBar.js
@@ -1,32 +1,3 @@
-/**
- * description：
- * @author Kevin
- * @date 2022/4/14
- */
-// import routers from "@/renderer/routers"
-// import {Menu} from "antd";
-// import {Link, useLocation} from "react-router-dom";
-//
-// function SideBar(){
-// 	const location = useLocation()
-// 	console.log(location,'0000000000000000000000000000')
-//
-// 	return (
-// 		<Menu
-// 			defaultSelectedKeys={['/banner-management']}
-// 			mode="inline"
-// 			theme="light"
-// 		>
-// 			{
-// 				routers.map((route,idx) => !route.hidden && <Menu.Item key={route.path} icon={route.icon}><Link to={route.path}>{route.title}</Link></Menu.Item>)
-// 			}
-// 		</Menu>
-// 	)
-// }
-//
-// export default SideBar
-
-
 import { Menu } from "antd"
 import React, { Fragment } from "react"
 import { Link, useLocation, useNavigate } from "react-router-dom"
