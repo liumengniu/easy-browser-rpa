276f76a6da736e41b2029a3e1d631556a84fffe2
lmn_741 <liu435839449>
2024-08-18 21:03:50 +0800

    修改readme

---
+++ src/interpreter/index.js
@@ -0,0 +1,29 @@
+/**
+ * 将"流程"解析为可执行的JS
+ * @author Kevin
+ * @Date:
+ */
+
+const interpreter = {
+
+	/**
+	 * 获取dom节点
+	 */
+	getDom: function (){
+		document.querySelector();
+	},
+	/**
+	 * 获取事件
+	 */
+	bindEvent: function (){
+
+	},
+	/**
+	 * 存储
+	 */
+	bindSave: function (){
+
+	}
+}
+
+export default interpreter
